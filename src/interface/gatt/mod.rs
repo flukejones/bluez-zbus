644c97ebf8799d2bdb51fbd8111b3f292a9b8911
Luke Jones <luke@ljones.dev>
2025-05-20 21:11:07 +0200

    Passthrough zbus feature enablement

--- src/interface/gatt/mod.rs
+++ src/interface/gatt/mod.rs
@@ -1,16 +1,25 @@
 mod types_;
 pub use types_::*;
 
+#[cfg(feature = "async-io")]
 mod application;
+#[cfg(feature = "async-io")]
 pub use application::*;
 
+#[cfg(feature = "async-io")]
 mod characteristic1;
+#[cfg(feature = "async-io")]
 pub use characteristic1::*;
 
+#[cfg(feature = "async-io")]
 mod descriptor;
+#[cfg(feature = "async-io")]
 pub use descriptor::*;
 
+#[cfg(feature = "async-io")]
 mod service1;
+#[cfg(feature = "async-io")]
 pub use service1::*;
 
+#[cfg(feature = "blocking-api")]
 pub mod blocking;
